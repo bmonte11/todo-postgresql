073769f8394df8e071bf3e2630195eb89bba7661
Brendan Monte <brendanmonte@Brendans-MacBook-Pro.local>
2021-04-26 16:41:46 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///test'
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
 class Todo(db.Model):
+    __tablename__ = 'todos'
+
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # def __init__(self, id, content, date_created):
+    #     self.id = id
+    #     self.content = content
+    #     self.date_created = date_created
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -61,6 +76,6 @@ def update(id):
         return render_template('update.html', task=task)
 
 
-print(app)
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
